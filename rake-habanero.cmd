47f5eb5f1e10b72e7f16c0be07b2a927de74fe06
Chillisoft <support@chillisoft.co.za>
2011-01-13 15:48:53 +0000

    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk@14980 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- rake-habanero.cmd
+++ rake-habanero.cmd
@@ -2,7 +2,7 @@ if exist temp goto dostuff
 md temp
 :dostuff
 cd temp
-svn co http://delicious:8080/svn/habanero/HabaneroCommunity/BuildScripts/habanero/%1 habanero
+svn co http://delicious:8080/svn/habanero/HabaneroCommunity/BuildScripts/habanero/%1 habanero --username chilli --password chilli --quiet
 cd habanero
 rmdir /s /q .svn
 rake --rakefile habanero-library-rakefile.rb
