a075f84c65d72c750ff49df09fda35dda671e651
Chillisoft <support@chillisoft.co.za>
2011-01-13 15:42:19 +0000

    More improvements to rake file
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk@14977 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- Rakefile.rb
+++ Rakefile.rb
@@ -10,25 +10,35 @@ task :build_smooth => [:clean_smooth,:copy_habanero_dlls_to_smooth_lib,:build_Fa
 $Nunit_path = "C:/Program Files (x86)/NUnit 2.5.6/bin/net-2.0/nunit-console-x86.exe"
 $Nunit_options = '/xml=nunit-result.xml'
 
+$MSBuild_path = "C:/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
+
+$habanero_version = "trunk"
+
+
+#--------------------------------build habanero-------------------------------------------
+
 exec :rake_habanero do |cmd|
    cmd.path_to_command = "rake-habanero.cmd"
+   cmd.parameters $habanero_version
 end
 
-#build_FakeBOsInSeperateAssembly tasks
-task :clean_FakeBOsInSeperateAssembly do #deletes bin folder
+
+#--------------------------------build FakeBOsInSeperateAssembly-------------------------
+
+task :clean_FakeBOsInSeperateAssembly do 
 	FileUtils.rm_rf 'temp/FakeBOsInSeperateAssembly/bin'
 end
 
-exec :checkout_FakeBOsInSeperateAssembly do |cmd| #command to check out FakeBOsInSeperateAssembly source using SVN
-	cmd.path_to_command = "../../../Utilities/BuildServer/Subversion/bin/svn.exe" # for some reason this doesn't pick up environment variables so I can't just use 'svn'
-	cmd.parameters %q(checkout "http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk/source/FakeBosInSeperateAssembly" temp/FakeBOsInSeperateAssembly/ --username chilli --password chilli) 
+exec :checkout_FakeBOsInSeperateAssembly do |cmd| 
+	cmd.path_to_command = "svn.exe" 
+	cmd.parameters %q(checkout "http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk/source/FakeBosInSeperateAssembly" temp/FakeBOsInSeperateAssembly/ --username chilli --password chilli --quiet ) 
 end
 
 msbuild :msbuild_FakeBOsInSeperateAssembly do |msb| #builds FakeBOsInSeperateAssembly with msbuild
   msb.targets :Rebuild
   msb.properties :configuration => :Debug
   msb.solution = "temp/FakeBOsInSeperateAssembly/FakeBOsInSeperateAssembly.sln"
-  msb.path_to_command = "C:/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
+  msb.path_to_command = $MSBuild_path
   msb.verbosity = "quiet"
 end
 
@@ -36,33 +46,33 @@ task :copy_dll_to_smooth_lib do
 	FileUtils.cp Dir.glob('temp/FakeBOsInSeperateAssembly/bin/FakeBosInSeperateAssembly.dll'), 'lib'
 end
 
-#build_smooth tasks
-task :clean_smooth do #deletes bin folder
+
+#-------------------------------build smooth itself ----------------------------------
+
+task :clean_smooth do 
 	FileUtils.rm_rf 'bin'
 end
 
-task :copy_habanero_dlls_to_smooth_lib  do #copies habanero DLLs to smooth lib
+task :copy_habanero_dlls_to_smooth_lib  do 
 	FileUtils.cp Dir.glob('temp/Habanero/bin/Habanero.Base.dll'), 'lib'
 	FileUtils.cp Dir.glob('temp/Habanero/bin/Habanero.Base.pdb'), 'lib'
 	FileUtils.cp Dir.glob('temp/Habanero/bin/Habanero.Base.xml'), 'lib'
 	FileUtils.cp Dir.glob('temp/Habanero/bin/Habanero.BO.dll'), 'lib'
 	FileUtils.cp Dir.glob('temp/Habanero/bin/Habanero.BO.pdb'), 'lib'
 	FileUtils.cp Dir.glob('temp/Habanero/bin/Habanero.BO.xml'), 'lib'
-	
 end
 
-
-msbuild :msbuild_smooth do |msb| #builds smooth with msbuild
+msbuild :msbuild_smooth do |msb| 
   msb.targets :Rebuild
   msb.properties :configuration => :Release
   msb.solution = "source/SmoothHabanero_2010.sln"
   msb.verbosity = "quiet"
-  msb.path_to_command = "C:/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
+  msb.path_to_command = $MSBuild_path
   end
 
 nunit :run_nunit do |nunit|
 	nunit.path_to_command = $Nunit_path
-	nunit.assemblies 'bin\Habanero.Smooth.Test.dll','bin\Habanero.Naked.Tests.dll', 'bin\Habanero.Fluent.Tests.dll' ,'bin\TestProject.Test.BO.dll','bin\TestProjectNoDBSpecificProps.Test.BO.dll' #this cannot be passed into the task as a global variable, unfortunately as it seems to be an array of sorts
+	nunit.assemblies 'bin\Habanero.Smooth.Test.dll','bin\Habanero.Naked.Tests.dll', 'bin\Habanero.Fluent.Tests.dll' ,'bin\TestProject.Test.BO.dll','bin\TestProjectNoDBSpecificProps.Test.BO.dll' 
 	nunit.options $Nunit_options
 end
 
@@ -70,7 +80,7 @@ task :clean_up do
 FileUtils.rm_rf 'temp'
 end
 
-exec :commit_lib do |cmd| #command to check out habanero source using SVN
-	cmd.path_to_command = "../../../Utilities/BuildServer/Subversion/bin/svn.exe" # for some reason this doesn't pick up environment variables so I can't just use 'svn'
-	cmd.parameters %q(ci -m autocheckin --username chilli --password chilli) 
+exec :commit_lib do |cmd|
+	cmd.path_to_command = "svn.exe" 
+	cmd.parameters %q(ci -m autocheckin --quiet --username chilli --password chilli) 
 end
--- rake-habanero.cmd
+++ rake-habanero.cmd
@@ -2,7 +2,7 @@ if exist temp goto dostuff
 md temp
 :dostuff
 cd temp
-svn co http://delicious:8080/svn/habanero/HabaneroCommunity/BuildScripts/habanero/trunk habanero
+svn co http://delicious:8080/svn/habanero/HabaneroCommunity/BuildScripts/habanero/%1 habanero
 cd habanero
 rmdir /s /q .svn
 rake --rakefile habanero-library-rakefile.rb
