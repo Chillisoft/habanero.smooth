0c3ce0e8e105d4c5c60a240100d0cad4f221e5cc
Peter Wiles <peter.wiles@chillisoft.co.za>
2011-01-14 15:02:34 +0000

    More work on rake
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk@15012 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,66 +1,84 @@
 require 'rake'
 require 'albacore'
-    
-task :default => [:clean_temp, :rake_habanero, :build, :clean_temp]
 
-task :build_FakeBOsInSeperateAssembly => [:clean_FakeBOsInSeperateAssembly,:checkout_FakeBOsInSeperateAssembly,:msbuild_FakeBOsInSeperateAssembly,:copy_dll_to_smooth_lib] 
+#______________________________________________________________________________
+#---------------------------------SETTINGS-------------------------------------
 
+#------------------------build settings--------------------------
+class Svn
+  include Albacore::Task
+  include Albacore::RunCommand
+  def execute
+	@command = "svn.exe"
+    run_command "svn", "--username chilli --password chilli --quiet --no-auth-cache"
+  end
+end
+
+msbuild_settings = {
+  :properties => {:configuration => :release},
+  :targets => [:clean, :rebuild]
+}
+
+Albacore.configure do |config|
+  config.log_level = :verbose
+  config.msbuild.verbosity = :quiet
+  config.nunit do |nunit|
+    nunit.command = "C:/Program Files (x86)/NUnit 2.5.6/bin/net-2.0/nunit-console-x86.exe"
+    nunit.options = ["/xml=nunit-result.xml"]
+  end
+end
+
+#------------------------dependency settings---------------------
+require 'rake-habanero.rb'
+$habanero_version = 'trunk'
+
+#------------------------project settings------------------------
+$basepath = 'http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk'
+$solution = 'source/SmoothHabanero_2010.sln'
+
+#______________________________________________________________________________
+#---------------------------------TASKS----------------------------------------
+
+task :default => [:build_all]
+task :build_all => [:clean_temp, :rake_habanero, :build, :clean_temp]
 task :build => [:clean, :updatelib, :build_FakeBOsInSeperateAssembly, :msbuild, :test, :commitlib]
+task :build_FakeBOsInSeperateAssembly => [:msbuild_FakeBOsInSeperateAssembly,:copy_dll_to_smooth_lib] 
 
-$Nunit_path = "C:/Program Files (x86)/NUnit 2.5.6/bin/net-2.0/nunit-console-x86.exe"
-$Nunit_options = '/xml=nunit-result.xml'
-
-$MSBuild_path = "C:/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
-
-$habanero_version = "trunk"
-
-#--------------------------------general tasks-------------------------------------------
+#------------------------general tasks---------------------------
 
 task :clean_temp do
 	FileUtils.rm_rf 'temp'
 end
 
-#--------------------------------build habanero-------------------------------------------
+#------------------------build FakeBOsInSeperateAssembly---------
 
-exec :rake_habanero do |cmd|
-   cmd.path_to_command = "rake-habanero.cmd"
-   cmd.parameters $habanero_version
-end
-
-
-#--------------------------------build FakeBOsInSeperateAssembly-------------------------
+$fakeBOsFolder = "temp/FakeBOsInSeperateAssembly"
 
 task :clean_FakeBOsInSeperateAssembly do 
-	FileUtils.rm_rf 'temp/FakeBOsInSeperateAssembly/bin'
+	FileUtils.rm_rf "#{$fakeBOsFolder}/bin"
 end
 
-exec :checkout_FakeBOsInSeperateAssembly do |cmd| 
-	cmd.path_to_command = "svn.exe" 
-	cmd.parameters %q(checkout "http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk/source/FakeBosInSeperateAssembly" temp/FakeBOsInSeperateAssembly/ --username chilli --password chilli --quiet --no-auth-cache) 
+svn :checkout_FakeBOsInSeperateAssembly => :clean_FakeBOsInSeperateAssembly do |s| 
+	s.parameters "co #{$basepath}/source/FakeBosInSeperateAssembly #{$fakeBOsFolder}"
 end
 
-msbuild :msbuild_FakeBOsInSeperateAssembly do |msb| #builds FakeBOsInSeperateAssembly with msbuild
-  msb.targets :Rebuild
-  msb.properties :configuration => :Debug
-  msb.solution = "temp/FakeBOsInSeperateAssembly/FakeBOsInSeperateAssembly.sln"
-  msb.path_to_command = $MSBuild_path
-  msb.verbosity = "quiet"
+msbuild :msbuild_FakeBOsInSeperateAssembly => :checkout_FakeBOsInSeperateAssembly do |msb| 
+	msb.update_attributes msbuild_settings
+    msb.solution = "#{$fakeBOsFolder}/FakeBOsInSeperateAssembly.sln"
 end
 
 task :copy_dll_to_smooth_lib do
-	FileUtils.cp Dir.glob('temp/FakeBOsInSeperateAssembly/bin/FakeBosInSeperateAssembly.dll'), 'lib'
+	FileUtils.cp Dir.glob("#{$fakeBOsFolder}/bin/FakeBosInSeperateAssembly.dll"), 'lib'
 end
 
-
-#-------------------------------build smooth itself ----------------------------------
+#------------------------build smooth itself --------------------
 
 task :clean do 
 	FileUtils.rm_rf 'bin'
 end
 
-exec :update_lib_from_svn do |cmd|
-	cmd.path_to_command = "svn.exe" 
-	cmd.parameters %q(update lib --username chilli --password chilli --quiet --no-auth-cache) 
+svn :update_lib_from_svn do |s|
+	s.parameters "update lib"
 end
 
 task :updatelib => :update_lib_from_svn do 
@@ -73,21 +91,14 @@ task :updatelib => :update_lib_from_svn do
 end
 
 msbuild :msbuild do |msb| 
-  msb.targets :Rebuild
-  msb.properties :configuration => :Release
-  msb.solution = "source/SmoothHabanero_2010.sln"
-  msb.verbosity = "quiet"
-  msb.path_to_command = $MSBuild_path
+	msb.update_attributes msbuild_settings
+	msb.solution = $solution
 end
 
 nunit :test do |nunit|
-	nunit.path_to_command = $Nunit_path
 	nunit.assemblies 'bin\Habanero.Smooth.Test.dll','bin\Habanero.Naked.Tests.dll', 'bin\Habanero.Fluent.Tests.dll' ,'bin\TestProject.Test.BO.dll','bin\TestProjectNoDBSpecificProps.Test.BO.dll' 
-	nunit.options $Nunit_options
 end
 
-
-exec :commitlib do |cmd|
-	cmd.path_to_command = "svn.exe" 
-	cmd.parameters %q(ci lib -m autocheckin --username chilli --password chilli --quiet --no-auth-cache) 
+svn :commitlib do |s|
+	s.parameters "ci lib -m autocheckin"
 end
--- rake-habanero.cmd
+++
@@ -1,6 +0,0 @@
-svn co http://delicious:8080/svn/habanero/HabaneroCommunity/BuildScripts/habanero/%1 temp/habanero --username chilli --password chilli --quiet --no-auth-cache
-cd temp/habanero
-rmdir /s /q .svn
-rake --rakefile habanero-library-rakefile.rb
-cd..
-cd..
