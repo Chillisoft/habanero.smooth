b49372aae78c7c89623a695c9e268fb2aeaa17aa
Brett Powell <brett.powell@chillisoft.co.za>
2011-05-12 16:45:12 +0000

    Removed unused references from the project
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/branches/v1.5_CF_Stargate@18722 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- source/Habanero.Smooth/AutoMapperExtensionMethods.cs
+++ source/Habanero.Smooth/AutoMapperExtensionMethods.cs
@@ -18,7 +18,6 @@
 // ---------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using Habanero.Smooth.ReflectionWrappers;
 using Habanero.Base;
