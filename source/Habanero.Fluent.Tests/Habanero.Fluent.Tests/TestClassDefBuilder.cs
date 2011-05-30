24bdb033311e4828ce897f12316c69585e504cd4
Andrew Russell <andrew.russell@chillisoft.co.za>
2011-05-30 18:05:11 +0000

    Added comments for work to be done next time.  It should be possible to build a classdef if a superclass is specified and there are no extra props on the sub class
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk@20123 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- source/Habanero.Fluent.Tests/Habanero.Fluent.Tests/TestClassDefBuilder.cs
+++ source/Habanero.Fluent.Tests/Habanero.Fluent.Tests/TestClassDefBuilder.cs
@@ -6,6 +6,11 @@ using Habanero.Smooth.Test;
 using NUnit.Framework;
 using TestProject.BO;
 
+
+//TODO andrew 30 May 2011: You should be able to build a classdef without any properties if it is a subclass.
+
+
+
 // ReSharper disable InconsistentNaming
 namespace Habanero.Fluent.Tests
 {
