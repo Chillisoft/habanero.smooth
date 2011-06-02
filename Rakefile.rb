0df9a695ad79eec223f18d90d77564b8f4554a2d
Brett Powell <brett.powell@chillisoft.co.za>
2011-06-02 08:32:24 +0000

    Removed the lib commit from rake so as to prevent the never ending builds being triggered on Jenkins.
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/branches/v1.5@20472 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- Rakefile.rb
+++ Rakefile.rb
@@ -42,7 +42,7 @@ desc "Rakes habanero, builds Smooth"
 task :build_all => [:clean_temp, :rake_habanero, :build, :clean_temp]
 
 desc "Builds Smooth, including tests"
-task :build => [:clean, :updatelib, :build_FakeBOs, :msbuild, :test, :commitlib]
+task :build => [:clean, :updatelib, :build_FakeBOs, :msbuild, :test]
 
 desc "builds the FakeBOs dll and copies to the lib folder"
 task :build_FakeBOs => [:msbuild_FakeBOsInSeperateAssembly,:copy_dll_to_smooth_lib] 
